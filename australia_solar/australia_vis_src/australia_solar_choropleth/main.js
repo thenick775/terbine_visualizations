5a87be4dc5d4bbe671427fcbeed0fb7bd8175528
Nicholas VanCise <40526638+thenick775@users.noreply.github.com>
2020-09-19 12:26:08 -0700

    Update main.js

--- australia_solar/australia_vis_src/australia_solar_choropleth/main.js
+++ australia_solar/australia_vis_src/australia_solar_choropleth/main.js
@@ -31,7 +31,7 @@ var map = L.map("map", {
     center: [-29.2744, 133.7751],
     zoom: zoomnum
 });
-var tiles = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+var tiles = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 });
 tiles.addTo(map);
