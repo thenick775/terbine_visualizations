6a83537f3f83b822812bb6a8604b0f58585872c6
Nick VanCise <nickvancise@MacBook-Pro.local>
2020-11-14 19:18:46 -0800

    update for air temp and water temp separate series (external data source)
    -Added other series for ancillary temperature from external source
    -Updated functionality to include legend
    -Fixed legend coloring usign regex
    -Added extra data files, updated desc/readme

--- river_discharge_wa/README.md
+++ river_discharge_wa/README.md
@@ -1,7 +1,7 @@
 #### Washington River Discharge Visualization
 ##### Written By: Nicholas VanCise
 
-![](https://github.com/thenick775/terbine_visualizations/blob/master/river_discharge_wa/graphics/demo.png)
+![](https://github.com/Terbine/projects/blob/master/river_discharge_wa/graphics/demo.png)
 
 [Try the visualization live!](https://raw.githack.com/thenick775/terbine_visualizations/master/river_discharge_wa/src/line.html)
 
@@ -14,12 +14,16 @@ How to operate:
 
 -Click on the paper icon in the upper left corner to view the raw data
 
+-Click on series in the legend to display and hide the individual lines
+
 
 This visualization describes the output, or river discharge, of Hutchinson Creek near Acme, Washington, United States.
 Water discharge readings at this station are collected using an acoustic doppler velocimeter (ADV) located near the source of Hutchinson Creek,
 where individual measurements are expressed in cubic feet per second (cfs).
 
-![](https://github.com/thenick775/terbine_visualizations/blob/master/river_discharge_wa/graphics/locations.png)
+![](https://github.com/Terbine/projects/blob/master/river_discharge_wa/graphics/locations.png)
+
+In addition to this, air and water temperature measurements are also collected at this site and are expressed in degrees Celsius.
 
 Data from these sensors, spread throughout the state of Washington, provide technical insight into the managing of water supplies, releases,
 and diversions in support of fish, farms, and domestic water needs across the state.
@@ -28,7 +32,7 @@ Here we can observe the high variation in river discharge from May through Octob
 and small time periods.
 
 This data can also be used to monitor erosion, predict and observe flooding events, and many other applications in the realm of the local
-water table and civil engineering fields. In addition these measurements can further be correlated with local weather and storm systems to
+water table and civil engineering fields. Where these measurements are correlated with local temperature and storm systems to
 illustrate the relationships between weather patterns and current stream conditions.
 
 Data Sources:
--- river_discharge_wa/src/main.go
+++ river_discharge_wa/src/main.go
@@ -18,6 +18,8 @@ import (
 
 var respath = "./line"
 var origfname = "./data/hutchinson.csv"
+var watertmpfname = "./data/hutchinson_water_temp.txt"
+var airtmpfname = "./data/hutchinson_air_temp.txt"
 
 type lineData struct {
 	date string
@@ -52,10 +54,28 @@ func getDates(d []lineData) []string {
 	return res
 }
 
-func dataFromFile() []lineData {
+func removeDuplicateValues(d []lineData) []lineData {
+	keys := make(map[string]bool)
+	list := []lineData{}
+
+	// If the key(values of the slice) is not equal
+	// to the already present value in new slice (list)
+	// then we append it. else we jump on another element.
+	for _, entry := range d {
+		if _, value := keys[entry.date]; !value {
+			keys[entry.date] = true
+			list = append(list, entry)
+		} else {
+			fmt.Println("found duplicate\n", entry)
+		}
+	}
+	return list
+}
+
+func dataFromFile(fname string) []lineData {
 	var res []lineData
 
-	file, err := os.Open(origfname)
+	file, err := os.Open(fname)
 	chkerr(err)
 	defer file.Close()
 
@@ -67,34 +87,66 @@ func dataFromFile() []lineData {
 			ldata, err := strconv.ParseFloat(lineval[2], 32)
 			chkerr(err)
 			res = append(res, lineData{date: lineval[0] + " " + lineval[1], data: float32(ldata)})
+		} else {
+			fmt.Println("missing value for date: " + lineval[0] + " " + lineval[1])
 		}
 	}
 	chkerr(scanner.Err())
 
 	sort.Sort(linesort(res))
 
+	res = removeDuplicateValues(res)
+
 	return res
 }
 
-func generateAreaC(d []lineData) *charts.Line {
+func generateAreaC(d []lineData, d2 []lineData, d3 []lineData) *charts.Line {
 	line := charts.NewLine()
 
-	line.SetXAxis(getDates(d)).AddSeries("River Discharge", getVals(d)).
-		SetSeriesOptions(
-			charts.WithLabelOpts(opts.Label{
-				Show: false,
-			}),
-			charts.WithAreaStyleOpts(opts.AreaStyle{
-				Color:   "#0465db",
-				Opacity: 0.3,
-			}),
-			charts.WithLineChartOpts(opts.LineChart{
-				Smooth: true,
-			}),
-			charts.WithLineStyleOpts(opts.LineStyle{
+	line.SetXAxis(getDates(d)).AddSeries("River Discharge", getVals(d),
+		charts.WithLabelOpts(opts.Label{
+			Show: false,
+		}),
+		charts.WithAreaStyleOpts(opts.AreaStyle{
+			Color:   "#0465db",
+			Opacity: 0.3,
+		}),
+		charts.WithLineChartOpts(opts.LineChart{
+			Smooth: true,
+		}),
+		charts.WithLineStyleOpts(opts.LineStyle{
+			Color: "#1cace6",
+		}),
+		charts.WithMarkPointStyleOpts(opts.MarkPointStyle{
+			Label: &opts.Label{
 				Color: "#1cace6",
-			}),
-		)
+			},
+		},
+		))
+
+	line.AddSeries("Water Temp", getVals(d2),
+		charts.WithLabelOpts(opts.Label{
+			Show: false,
+		}),
+		charts.WithLineChartOpts(opts.LineChart{
+			Smooth:     true,
+			YAxisIndex: 1,
+		}),
+		charts.WithLineStyleOpts(opts.LineStyle{
+			Color: "#43e7f0",
+		}))
+
+	line.AddSeries("Air Temp", getVals(d3),
+		charts.WithLabelOpts(opts.Label{
+			Show: false,
+		}),
+		charts.WithLineChartOpts(opts.LineChart{
+			Smooth:     true,
+			YAxisIndex: 1,
+		}),
+		charts.WithLineStyleOpts(opts.LineStyle{
+			Color: "#DF013A",
+		}))
 
 	line.SetGlobalOptions(
 		charts.WithInitializationOpts(opts.Initialization{
@@ -111,21 +163,21 @@ func generateAreaC(d []lineData) *charts.Line {
 		}),
 		charts.WithDataZoomOpts(opts.DataZoom{
 			Type:       "inside",
-			Start:      88,
-			End:        100,
+			Start:      23,
+			End:        35,
 			XAxisIndex: []int{0},
 		}),
 		charts.WithDataZoomOpts(opts.DataZoom{
 			Type:       "slider",
-			Start:      88,
-			End:        100,
+			Start:      23,
+			End:        35,
 			XAxisIndex: []int{0},
 		}),
 		charts.WithTooltipOpts(opts.Tooltip{
 			Trigger:   "axis",
 			TriggerOn: "click",
 			Show:      true,
-			Formatter: "Date: {b}<br/>Obs: {c} (cfs)",
+			Formatter: "Date: {b}<br/>Discharge: {c} (cfs)<br/>Air Temp: {c2} °C<br/>Water Temp: {c1} °C",
 		}),
 		charts.WithToolboxOpts(opts.Toolbox{
 			Show:   true,
@@ -143,7 +195,11 @@ func generateAreaC(d []lineData) *charts.Line {
 				nil,
 			},
 		}),
+		charts.WithLegendOpts(opts.Legend{
+			Show: true,
+		}),
 	)
+
 	return line
 }
 
@@ -154,12 +210,14 @@ func chkerr(err error) {
 }
 
 func main() {
-	d := dataFromFile()
+	d := dataFromFile(origfname)
+	d2 := dataFromFile(watertmpfname)
+	d3 := dataFromFile(airtmpfname)
 	respath := "./line.html"
 
 	page := components.NewPage()
 	page.AddCharts(
-		generateAreaC(d),
+		generateAreaC(d, d2, d3),
 	)
 	f, err := os.Create(respath)
 	chkerr(err)
@@ -167,13 +225,13 @@ func main() {
 	f.Close()
 
 	//make readonly, and resize edits
-	reptoolb, err := regexp.Compile("toolbox: {.*}}}") //find toolbox
+	reptoolb, err := regexp.Compile(`"toolbox":{.*}}}`) //find toolbox
 	chkerr(err)
-	genname, err := regexp.Compile("myChart_.* =") //find script to replace
+	genname, err := regexp.Compile("goecharts_.* =") //find script to replace
 	chkerr(err)
 	addresponsiveness, err := regexp.Compile(`\s+</script>`) //find script to replace
 	chkerr(err)
-	fixslidercolors, err := regexp.Compile(`dataZoom:\[.*\]`) //find toolbox
+	fixslidercolors, err := regexp.Compile(`"dataZoom":\[.*?\],`) //find toolbox
 	chkerr(err)
 	fixtitle, err := regexp.Compile(`<title>.*</title>`) //find toolbox
 	chkerr(err)
@@ -183,20 +241,35 @@ func main() {
 	chkerr(err)
 	contresize, err := regexp.Compile(`<div class="container">`) //fix dynamic resize vertical
 	chkerr(err)
+	multiyaxis, err := regexp.Compile(`"yAxis":\[.*?};`)
+	chkerr(err)
+	addcolors, err := regexp.Compile(`option_.* = {`) //fix legend colors using global echarts opt
+	chkerr(err)
 
 	b, err := ioutil.ReadFile(respath)
 	chkerr(err)
 
 	oldtoolbox := reptoolb.FindAllString(string(b), -1)
+
 	oldslider := fixslidercolors.FindAllString(string(b), -1)
+	oldsliderstr := oldslider[0][0 : len(oldslider[0])-1]
+	oldcolor := addcolors.FindAllString(string(b), -1)
+	fmt.Println("oldcolor")
+	fmt.Println(oldcolor)
+
 	namestr := genname.FindAllString(string(b), -1)
 	namestrsing := namestr[0][:len(namestr[0])-2]
 	var newall []byte
 
-	if len(oldtoolbox) == 1 && len(oldslider) == 1 { //if we only have one result, edit the html
-		newtoolbox := oldtoolbox[0][:len(oldtoolbox[0])-3] + `,"readOnly":true}}}`
-		newslider := oldslider[0][:len(oldslider[0])-2] + `,handleColor: '#eb8146',textStyle:{color:"#ffffff"},dataBackground:{lineStyle:{color:"#ffffff"},areaStyle:{color:"#ffffff"}}}]`
+	fmt.Println(oldtoolbox)
+	fmt.Println(oldsliderstr)
+	fmt.Println(namestr)
+	fmt.Println(namestrsing)
 
+	if len(oldslider) == 1 { //if we only have one result, edit the html
+		newtoolbox := oldtoolbox[0][:len(oldtoolbox[0])-3] + `,"readOnly":true}}}`
+		newslider := oldslider[0][:len(oldslider[0])-3] + `,handleColor: '#eb8146',textStyle:{color:"#ffffff"},dataBackground:{lineStyle:{color:"#ffffff"},areaStyle:{color:"#ffffff"}}}],`
+		newcolor := oldcolor[0] + `"color":["#1cace6","#43e7f0","#DF013A"],`
 		newall = reptoolb.ReplaceAll(b, []byte(newtoolbox))
 		newall = fixslidercolors.ReplaceAll(newall, []byte(newslider))
 		newall = fixtitle.ReplaceAll(newall, []byte(`<title>River Discharge</title>`))
@@ -204,6 +277,8 @@ func main() {
 		newall = htmlresize.ReplaceAll(newall, []byte(`<html style="width: 100%;height:100%;">`))
 		newall = bodyresize.ReplaceAll(newall, []byte(`<body style="width: 100%;height:100%;">`))
 		newall = contresize.ReplaceAll(newall, []byte(`<div class="container" style="width: 100%;height:100%;bottom:2%">`))
+		newall = multiyaxis.ReplaceAll(newall, []byte(`"yAxis":[[{"scale":true}],[{"scale":true}]]};`))
+		newall = addcolors.ReplaceAll(newall, []byte(newcolor))
 
 		fmt.Println(namestrsing)
 		newendscript := addresponsiveness.FindAllString(string(newall), -1)
